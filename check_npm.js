c5311a2f82b1810e73c1b0b20fc8d3d4b673bb81
PolGuixe <pol@zetoff.com>
2016-11-15 16:54:41 +0100

    relaxed material-ui version

--- check_npm.js
+++ check_npm.js
@@ -1,6 +1,6 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
-  'material-ui': '0.15.4'
+  'material-ui': '>=0.16.1'
 }, 'zetoff:accounts-material-ui');
 
 const MaterialUI = require('material-ui');
