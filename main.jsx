f0b2f39965d7696aae9719746aebe0877e9d2cdc
PolGuixe <pol@zetoff.com>
2016-12-04 23:03:32 +0100

    moved formMessage to a SnackBar

--- main.jsx
+++ main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Accounts, STATES} from './fix.js'; // TODO: back to normal once std:accounts-ui is fixed
-import {RaisedButton, FlatButton, FontIcon, TextField, Divider} from 'material-ui';
+import {RaisedButton, FlatButton, FontIcon, TextField, Divider, Snackbar} from 'material-ui';
 import {socialButtonsColors, socialButtonIcons} from './social_buttons_config';
+import {green500, red500, yellow600, lightBlue600} from 'material-ui/styles/colors';
 
 /**
  * Form.propTypes = {
@@ -181,7 +182,66 @@ class SocialButtons extends Accounts.ui.SocialButtons {
 
 	}
 }
-class FormMessage extends Accounts.ui.FormMessage {}
+
+
+
+class FormMessage extends Accounts.ui.FormMessage {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.message) {
+      this.setState({open: true});
+    }
+  }
+
+  handleRequestClose() {
+    this.setState({open: false});
+  };
+
+  render() {
+    const {message, type} = this.props;
+    let bodyStyle;
+    switch (type) {
+      case 'warning':
+        bodyStyle = {
+          backgroundColor: yellow600
+        }
+        break;
+      case 'success':
+        bodyStyle = {
+          backgroundColor: green500
+        }
+        break;
+      case 'error':
+        bodyStyle = {
+          backgroundColor: red500
+        }
+        break;
+      case 'info':
+        bodyStyle = {
+          backgroundColor: lightBlue600
+        }
+        break;
+    }
+
+    return message
+      ? (<Snackbar
+        open={this.state.open}
+        message={message}
+        bodyStyle={bodyStyle}
+        action="OK"
+        autoHideDuration={4000}
+        onActionTouchTap={this.handleRequestClose.bind(this)}
+        onRequestClose={this.handleRequestClose.bind(this)}/>)
+      : null;
+  }
+}
+
 // Notice! Accounts.ui.LoginForm manages all state logic at the moment, so avoid overwriting this
 // one, but have a look at it and learn how it works. And pull requests altering how that works are
 // welcome. Alter provided default unstyled UI.
