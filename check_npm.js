54addb7e7b18dbf8b9790b2879f7f73ebc5ddff6
ochicf <oscarchicfarre@gmail.com>
2018-07-04 17:16:59 +0200

    remove dependencies

--- check_npm.js
+++
@@ -1,6 +0,0 @@
-import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
-checkNpmVersions({
-  'material-ui': '>=0.16.x'
-}, 'zetoff:accounts-material-ui');
-
-const MaterialUI = require('material-ui');
