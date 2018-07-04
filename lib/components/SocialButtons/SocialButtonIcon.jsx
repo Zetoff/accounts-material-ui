62f3ff03e3ca2176a8212d36793d89f928c43c53
ochicf <oscarchicfarre@gmail.com>
2018-07-04 17:44:02 +0200

    if no icon provided, render regular SocialButton

--- lib/components/SocialButtons/SocialButtonIcon.jsx
+++ lib/components/SocialButtons/SocialButtonIcon.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import MuiIconButton from '@material-ui/core/IconButton';
+import MuiTooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core/styles';
+
+import SocialButton from './SocialButton';
 
 const styles = () => ({
   button: {},
@@ -31,27 +34,42 @@ const styles = () => ({
   }
 });
 
-const SocialButtonIcon = ({
-  id: serviceId,
-  className,
-  Icon,
-  label,
-  classes,
-  ...props
-}) => (
-  <MuiIconButton
-    color="primary"
-    className={[
-      className,
-      classes.button,
-    ].join(' ')}
-    classes={{
-      colorPrimary: classes[serviceId],
-    }}
-    {...props}
-  >
-    <Icon className={classes.icon} />
-  </MuiIconButton>
-);
+const SocialButtonIcon = ({ Icon, ...props }) => {
+  const {
+    id: serviceId,
+    className,
+    label,
+    classes,
+    ...iconButtonProps
+  } = props;
+  if (!Icon) {
+    return (
+      <SocialButton
+        {...props}
+        classes={{
+          ...classes,
+          [serviceId]: undefined,
+        }}
+      />
+    );
+  }
+  return (
+    <MuiTooltip title={label}>
+      <MuiIconButton
+        color="primary"
+        className={[
+          className,
+          classes.button,
+        ].join(' ')}
+        classes={{
+          colorPrimary: classes[serviceId],
+        }}
+        {...iconButtonProps}
+      >
+        <Icon className={classes.icon}/>
+      </MuiIconButton>
+    </MuiTooltip>
+  );
+};
 
 export default withStyles(styles)(SocialButtonIcon);
